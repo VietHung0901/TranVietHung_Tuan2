71d15b8e0acadf883d5fe6132ca8c6745de67826
Tran Viet Hung <hungtgdd68@gmail.com>
2024-05-30 09:22:43 +0700

    Bài tập tuần 2

---
+++ src/main/java/com/example/ngay28/TranVietHungTuan2Application.java
@@ -0,0 +1,13 @@
+package com.example.ngay28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranVietHungTuan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranVietHungTuan2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ngay28/TranVietHungTuan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngay28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranVietHungTuan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
